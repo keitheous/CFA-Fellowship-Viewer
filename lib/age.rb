d15d68a999010f325a706d8a7d06016cdcda7fab
keitheous <keithchongwy@gmail.com>
2018-06-26 00:49:17 +1000

    Managed interactions between Age and Fellowship classes

--- lib/age.rb
+++ lib/age.rb
@@ -1,8 +1,14 @@
 class Age
-  attr_reader :min_age, :max_age
+  attr_reader :min, :max
 
   def initialize(ages)
-    @min = ages.min
-    @max = ages.max
+    @min = ages.max
+    @max = ages.min
+  end
+
+  def calculate_average_per_group(group)
+    ages_in_group = group.collect{|fellow| fellow.age}
+
+    ages_in_group.sum / ages_in_group.size
   end
 end
--- lib/fellowship.rb
+++ lib/fellowship.rb
@@ -3,17 +3,47 @@ require_relative 'age'
 require 'json'
 
 class Fellowship
-  attr_reader :groups, :dataset
+  attr_reader :groups, :dataset, :age
 
   def initialize(dataset = JsonParser.new.load_fellows, age = Age)
-    @age     = age.new(dataset.collect{|x| x.age})
+    @age     = age.new(dataset.collect{|data| data.age})
+
+    @dataset = dataset
+    @groups  = {}
+  end
+
+  def sort_groups
+    dataset.each do |data|
+      distribute_fellow(data)
+    end
+
+    set_average_ages
+    self
+  end
+
+  def max_age
+    age.max
   end
 
   def min_age
     age.min
   end
 
-  def max_age
-    age.max
+  def set_average_ages
+    groups.keys.each do |key|
+      average = age.calculate_average_per_group(groups[key][:fellows])
+
+      groups[key].merge!(:average_age => average)
+    end
+  end
+
+  private
+
+  def distribute_fellow(fellow)
+    unless groups.keys.include?(fellow.fellowship)
+      groups[fellow.fellowship] = {:fellows => [fellow]}
+    else
+      groups[fellow.fellowship][:fellows] << fellow
+    end
   end
 end
