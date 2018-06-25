4c07d1a512344956117df83d224eb667eb5a4313
keitheous <keithchongwy@gmail.com>
2018-06-25 23:09:00 +1000

    Created JsonParser to execute json-related responsibilities

---
+++ lib/json_parser.rb
@@ -0,0 +1,29 @@
+require_relative 'fellow'
+require 'pry'
+require 'json'
+
+class JsonParser
+  attr_reader :dataset
+
+  def initialize(dataset = JSON.parse(File.read("data/fellows_dataset.json")))
+    @dataset = dataset
+  end
+
+  def load_fellows(fellow_object = Fellow)
+    dataset.map do |data|
+      fellow_object.new(
+        :id         => data['id'],
+        :active     => data['isActive'],
+        :age        => data['age'],
+        :fellowship => data['fellowship'],
+        :first_name => data['name']['first'],
+        :last_name  => data['name']['last'],
+        :email      => data['email'],
+        :about      => data['about'],
+        :registered => data['registered']
+      )
+    end
+
+    self
+  end
+end
