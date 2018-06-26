4a7f9cda44cb3e6a06ab988cdaa9296914dea9b6
keitheous <keithchongwy@gmail.com>
2018-06-27 00:31:51 +1000

    Expended Fellowship & Age classes to add new Fellow

--- lib/age.rb
+++ lib/age.rb
@@ -6,6 +6,13 @@ class Age
     @max = ages.max
   end
 
+  def add_new(age)
+    @max = age if age > self.max
+    @min = age if age < self.min
+
+    self
+  end
+
   def calculate_average_per_group(group)
     ages_in_group = group.collect{|fellow| fellow.age}
 
--- lib/fellowship.rb
+++ lib/fellowship.rb
@@ -1,6 +1,8 @@
 require_relative 'json_parser'
 require_relative 'age'
+require_relative 'fellow'
 require 'json'
+require 'pry'
 
 class Fellowship
   attr_reader :groups, :dataset, :age
@@ -20,6 +22,14 @@ class Fellowship
     self
   end
 
+  def add_fellow(fellow_args = {}, fellow_class = Fellow)
+    return require_fellow_details if fellow_args.empty?
+
+    new_fellow = fellow_class.new(fellow_args)
+
+    create_new_profile(new_fellow)
+  end
+
   def max_age
     age.max
   end
@@ -29,6 +39,20 @@ class Fellowship
   end
 
   private
+
+  def create_new_profile(fellow)
+    age.add_new(fellow.age)
+
+    dataset << fellow
+
+    distribute_fellow(fellow)
+
+    set_average_ages
+  end
+
+  def require_fellow_details
+    puts "Details are required to register new Fellow."
+  end
 
   def distribute_fellow(fellow)
     if existing_fellowship?(fellow.fellowship)
